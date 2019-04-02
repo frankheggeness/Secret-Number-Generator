6249f914a897bfc568cf0f688b2f59db98dd7fa5
Frank <fr.heggeness@gmail.com>
2019-04-01 18:16:03 -1000

    secretnum 1

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
+  const secretNumber = Math.floor(Math.random()*1000000);
+
+  let secretNumberGenerator = function(){
+
+      return secretNumber;
   
+  }
+  return secretNumberGenerator
 };
